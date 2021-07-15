191026734a2d41b1f213472809f3f68e8489f767
joypauls <joypaulsen3@gmail.com>
2021-07-14 23:06:21 -0500

    commit updates

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -49,7 +49,7 @@ const theme = merge(tailwind, {
       MozOsxFontSmoothing: `grayscale`,
     },
     p: {
-      fontSize: [1, 1, 2],
+      fontSize: [1, 2, 3],
       letterSpacing: `-0.003em`,
       lineHeight: `body`,
       "--baseline-multiplier": 0.179,

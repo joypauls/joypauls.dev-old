9130fd3b8a0d12b43bfd96de5690752b13e7daf9
joypauls <joypaulsen3@gmail.com>
2021-08-27 20:19:05 -0500

    netlify modification

--- src/components/contact.js
+++ src/components/contact.js
@@ -15,21 +15,41 @@ const encode = (data) => {
     .join("&");
 }
 
+const validateEmail = (s) => {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(s);
+}
+
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+
+  const resetForm = () => {
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
   
   const handleSubmit = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", "name": name, "email": email, "message": message })
-    })
-      .then(() => alert(`Sent!\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}\n`))
-      .catch(error => alert(error));
-    e.preventDefault();
-    // alert(`Name: ${name}\nEmail: ${email}\nComment: ${comment}\n\nNot implemented oops!\nSee why here https://youtu.be/dQw4w9WgXcQ`);
+    if (name === "" || email === "" || message === "") {
+      alert("Please fill out all fields, thank you!")
+    } if (validateEmail(email)) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", "name": name, "email": email, "message": message })
+      })
+        .then(() => {
+          alert(`Sent!\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}\n`)
+          resetForm();
+        })
+        .catch(error => alert(error));
+      e.preventDefault();
+      // alert(`Name: ${name}\nEmail: ${email}\nComment: ${comment}\n\nNot implemented oops!\nSee why here https://youtu.be/dQw4w9WgXcQ`);
+    } else {
+      // alert("That doesn't look like a valid email");
+    }
   }
 
   return(
